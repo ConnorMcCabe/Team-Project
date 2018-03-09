8d015ceea9240c33e945ba12d596e4cafd2dc994
GavinMcPaul <33657854+GavinMcPaul@users.noreply.github.com>
2018-03-09 19:44:51 +0000

    Add files via upload

--- New Project Code/src/classes/Order.java
+++ New Project Code/src/classes/Order.java
@@ -1,10 +1,76 @@
 package classes;
 
-public class Order {
+import java.sql.ResultSet;
+import java.sql.*;
+import com.mysql.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
+public class Order extends Order_Item {
+
+		//CLASS ATTRIBUTES 
+		private int orderNum;
+		private int tableNum;
+		private boolean status;
+		private String time;
+		
+		//SQL VARIABLES
+		private String url = "jdbc:mysql://localhost:3306/";
+		private String dbName = "fast_food";
+		private String driver = "com.mysql.jdbc.Driver";
+		private String userName = "root";
+		private String password = "password";
+		private Statement statement = null;   
+		private ResultSet resultSet = null;
+		
+		//DEFAULT CODE TO CONNECT TO THE DATABASE
+		
+		
+		//CONSTRUCTORS 
+		public Order(int orderNumIn, int tableNumIn, boolean statusIn, String timeIn)
+		{
+			orderNum = orderNumIn;
+			tableNum = tableNumIn;
+			status = statusIn;
+			time = timeIn;
+		}
+		
+		public Order()
+		{
+			
+		}
+		
+		//SETTERS AND GETTERS
+		public int getOrderNum() 
+		{
+			return orderNum;
+		}
+		
+		public int getTableNum()
+		{
+			return tableNum;
+		}
+		
+		public void setTableNum(int tableNum) 
+		{
+			this.tableNum = tableNum;
+		}
+		public boolean isStatus() 
+		{
+			return status;
+		}
+		
+		public void setStatus(boolean status) 
+		{
+			this.status = status;
+		}
+		
+		public String isTime() 
+		{
+			return time;
+		}
+		
 	}
 
-}
+
