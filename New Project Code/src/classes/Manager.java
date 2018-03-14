d57bdf5fd21fed353d8016b3c986de4d16b89ddb
GavinMcPaul <33657854+GavinMcPaul@users.noreply.github.com>
2018-03-14 19:34:38 +0000

    Add files via upload

--- New Project Code/src/classes/Manager.java
+++ New Project Code/src/classes/Manager.java
@@ -45,12 +45,13 @@ public class Manager {
 		private JPanel panelMenu,panelChoose,panel_Order;
 	
 		private JPasswordField enterPassword;
+		private JTextField add_codeIn;
+		private JTextField add_nameIn;
+		private JTextField add_priceIn;
+		private JTextField add_sectionIn;
+		private JTextField add_descripIn;
+		private JTextField add_alergyIn;		
 
-		//private JPasswordField enterPassword;
-		
-
-
-	    
 	/**
 	 * Launch the application.
 	 */
@@ -78,6 +79,10 @@ public class Manager {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		//JRAME CREATION AND JPANEL CODE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -93,11 +98,151 @@ public class Manager {
 		panelChoose.setLayout(null);
 		panelChoose.setVisible(false);
 		
-		final JPanel panel_Order = new JPanel();
-		frame.getContentPane().add(panel_Order, "name_10478840946849");
-		panel_Order.setLayout(null);
-		panel_Order.setVisible(false);
+		final JPanel panel_Add = new JPanel();
+		frame.getContentPane().add(panel_Add, "name_10478840946849");
+		panel_Add.setLayout(null);
+		panel_Add.setVisible(false);
 		
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		//PANEL ADD CODE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+		JLabel add_code = new JLabel("Product Code:");
+		add_code.setBounds(12, 46, 94, 16);
+		panel_Add.add(add_code);
+		
+		JLabel add_name = new JLabel("Name:");
+		add_name.setBounds(12, 75, 56, 16);
+		panel_Add.add(add_name);
+		
+		JLabel add_price = new JLabel("Price:");
+		add_price.setBounds(12, 104, 56, 16);
+		panel_Add.add(add_price);
+		
+		JLabel add_descrip = new JLabel("Description:");
+		add_descrip.setBounds(238, 46, 94, 16);
+		panel_Add.add(add_descrip);
+		
+		JLabel add_section = new JLabel("Section");
+		add_section.setBounds(12, 133, 56, 16);
+		panel_Add.add(add_section);
+		
+		JLabel add_alergy = new JLabel("Alergy:");
+		add_alergy.setBounds(238, 133, 56, 16);
+		panel_Add.add(add_alergy);
+		
+		add_codeIn = new JTextField();
+		add_codeIn.setBounds(110, 43, 116, 22);
+		panel_Add.add(add_codeIn);
+		add_codeIn.setColumns(10);
+		
+		add_nameIn = new JTextField();
+		add_nameIn.setBounds(110, 72, 116, 22);
+		panel_Add.add(add_nameIn);
+		add_nameIn.setColumns(10);
+		
+		add_priceIn = new JTextField();
+		add_priceIn.setBounds(110, 101, 116, 22);
+		panel_Add.add(add_priceIn);
+		add_priceIn.setColumns(10);
+		
+		add_sectionIn = new JTextField();
+		add_sectionIn.setBounds(110, 130, 116, 22);
+		panel_Add.add(add_sectionIn);
+		add_sectionIn.setColumns(10);
+		
+		add_descripIn = new JTextField();
+		add_descripIn.setBounds(238, 63, 170, 62);
+		panel_Add.add(add_descripIn);
+		add_descripIn.setColumns(10);
+		
+		add_alergyIn = new JTextField();
+		add_alergyIn.setBounds(238, 150, 170, 62);
+		panel_Add.add(add_alergyIn);
+		add_alergyIn.setColumns(10);
+		
+		//AL
+		//
+		JButton add_OK = new JButton("OK");
+		add_OK.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				try 
+				{		
+					 String code1 = add_codeIn.getText();
+					 String name1 = add_nameIn.getText();
+					 String price1 = add_priceIn.getText();
+					 String descrip1 = add_descripIn.getText();
+					 String section1 = add_sectionIn.getText();
+					 String alergy1 = add_alergyIn.getText();
+						
+					//CODE TO VERFIY THE EMAIL ADDRESS AND PASSWORD FROM THE DATABASE 
+					Class.forName("com.mysql.jdbc.Driver");
+			        Connection conn = DriverManager.getConnection(url+dbName,userName,passwordDB);
+			        statement=conn.createStatement(); 
+			       	        
+			        if(code1 != null)
+			        {
+			        	
+			        	String code = new String(code1);
+			        	String query1 ="INSERT INTO Product values (?,?,?,?,?,?)";
+			        	
+			        	PreparedStatement ps = conn.prepareStatement(query1);
+			        	ps.setString(1, code1);
+			        	ps.setString(2, name1);
+			        	ps.setString(3, price1);
+			        	ps.setString(4, descrip1);
+			        	ps.setString(5, section1);
+			        	ps.setString(6, alergy1);
+
+			        	//ResultSet rs;
+			        	ps.execute();
+			        	
+			        	 
+			        	// if(ps.next())
+			        	// {  
+			           //      JOptionPane.showMessageDialog(null, "New Product Was Added to Menu","Product Added", JOptionPane.INFORMATION_MESSAGE);
+
+			        	// }
+			        	 
+			        	 //else
+			        	// {
+			        	//	 JOptionPane.showMessageDialog(null, "Incorrect Password or Email, logIn","Please Try again", JOptionPane.ERROR_MESSAGE);
+
+			        	// }
+			        	 
+			        	 //ps.close();
+			        	// rs.close();
+			        	 conn.close();
+			        }
+				}catch(Exception ee) 
+				{
+					System.out.println(ee);
+
+				}
+				
+			}
+		});
+		//
+		
+		add_OK.setBounds(272, 225, 97, 25);
+		panel_Add.add(add_OK);
+		
+		JLabel lblPleaseEnterProduct = new JLabel("Please Enter Product Detilas");
+		lblPleaseEnterProduct.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblPleaseEnterProduct.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseEnterProduct.setBounds(50, 13, 319, 16);
+		panel_Add.add(lblPleaseEnterProduct);
+		panel_Add.setVisible(false);
+		
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		//PANEL MENU CODE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		JLabel lblNewLabel = new JLabel("Mr Chippy");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 25));
@@ -125,11 +270,11 @@ public class Manager {
 		btnLogin.setBounds(135, 189, 97, 25);
 		panelMenu.add(btnLogin);
 		
-		
-		
+		//AL
+		// 
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				panelChoose.setVisible(true);
+				//panelChoose.setVisible(true);
 
 				try 
 				{		
@@ -199,20 +344,27 @@ public class Manager {
 				}
 				
 			}
-			
+			//
 		});
 
-		
-		
-		
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		//PANEL CHOOSE CODE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		//
+		// AL
 		JButton add = new JButton("Add Item");
 		add.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
-				
+				panelChoose.setVisible(false);
+				panel_Add.setVisible(true);
+
 			}
 		});
+		//
 		
 		add.setBounds(154, 80, 115, 33);
 		panelChoose.add(add);
@@ -248,6 +400,8 @@ public class Manager {
 		lblPleaseSelectAn.setBounds(55, 13, 331, 51);
 		panelChoose.add(lblPleaseSelectAn);
 		
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		
 	}
 }
