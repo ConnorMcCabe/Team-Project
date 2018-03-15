8b398c87b90b521cd22ab5a9db818a0d79b8a904
ConnorMcCabe <34235565+ConnorMcCabe@users.noreply.github.com>
2018-03-15 16:48:32 +0000

    Update Order_Item.java

--- New Project Code/src/classes/Order_Item.java
+++ New Project Code/src/classes/Order_Item.java
@@ -1,79 +1,76 @@
 package Classes;
 // Connor McCabe
 // The Chippy - Order Item class
-// 10/03/18
+// 15/03/18
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Vector;
-
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
 
 public class Order_Item 
 {
 	//Instance Variables
 	private int productCode;
-	private int quantity;
-	private int orderNum;
-	private double totalPrice;
-
+	private int orderQuantity;
+	private Double orderPrice;
+	private String orderName;
 	//SQL Variables
 	ResultSet resultSet = null;
 	
 	//private String
 	public Order_Item()
-	{
-	
-	}
+	{}
 		
-	public Order_Item(int productCode, int quantity, double totalPrice)
+//	public Order_Item(int productCode, String orderName ,double orderPrice, int orderQuantity)
+//	{
+//		this.productCode=productCode;
+//		this.orderQuantity=orderQuantity;
+//		this.orderPrice=orderPrice;
+//		this.orderName=orderName;
+//	}
+
+	public Order_Item(int productCode , int orderQuantity, double orderPrice)
 	{
 		this.productCode=productCode;
-		this.quantity=quantity;
-		this.totalPrice=totalPrice;
+		this.orderQuantity=orderQuantity;
+		this.orderPrice=orderPrice;
+		this.orderName=orderName;
 	}
-	public Vector getOrderItems(int j)
-	{
-		ResultSet rs = null;
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			java.sql.Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Fast_Food","root","password");
-			Statement stmt = con.createStatement();
-		
-			String sqlString = "select Product_Code, Price from product";
-			stmt.executeQuery(sqlString);
-			Vector vector = new Vector();
-			Vector<String> orderItem = new Vector<String>();
-			int i=0;
-			while(rs.next())
-			{
-				Vector<String> items = new Vector <String>();
-				rs.getInt("ProductCode");
-				rs.getInt("Quantity");
-				rs.getDouble("Total_Price");
-			}
-			return vector;
-		}catch(Exception e)
-		{
-			System.out.print("error \t ");
-		}
-		return null;
-	}
-	
 	//Getter Methods
-	public int getProductCode()
-	{
-		return productCode;
-	}
+		public int getProductCode()
+		{
+			return productCode;
+		}
+		public int getOrderQuantity()
+		{
+			return orderQuantity;
+		}
+		public double getOrderPrice()
+		{		
+			return orderPrice;
+		}		
+		public String getOrderName()
+		{
+			return orderName;
+		}
 	
-	public int getQuantity()
-	{
-		return quantity;
-	}
-	
-	public double getTotalPrice()
-	{		
-		return totalPrice;
-	}
-	
+		public ArrayList<Order_Item> TotalOrder()      { 
+		      ArrayList<Order_Item> TotalOrder = new ArrayList<Order_Item>();
+		      try {
+		    	  Class.forName("com.mysql.jdbc.Driver");  
+		  		java.sql.Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Fast_Food","root","password"); 
+		         
+		         while ( resultSet.next() )    {
+//		            Order_Item nextItem = new Order_Item(resultSet.getInt("productCode"),resultSet.getString("orderName"), 
+//		            		resultSet.getDouble("Price"),resultSet.getInt("orderQuantity"));           
+		        	 Order_Item nextItem = new Order_Item(resultSet.getInt("productCode"),resultSet.getInt("orderQuantity"),resultSet.getDouble("orderPrice")); 
+		            TotalOrder.add(nextItem);
+		         }
+		         con.close();
+		      } 
+		      catch (Exception e) {
+		         e.printStackTrace();
+		      }
+		      return TotalOrder;     		
+}		
 }
